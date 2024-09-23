dc6c4ef01cb90d7f674dbceb81a401a67f9731f3
xiayulin123 <xiayulinsky@gmail.com>
2024-09-22 20:08:34 -0400

    processing the website ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,35 @@
+import LeftSideBar from "@/components/LeftSideBar";
+import MobileNav from "@/components/MobileNav";
+import RightSideBar from "@/components/RightSideBar";
+import Image from 'next/image'
+
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
     return (
-      <div>
-        <main>
-            <p className="text-white-1">LEFT SIDEBAR</p>
-            {children}
-            <p className="text-white-1">RIGHT SIDEBAR</p>
+      <div className="flex flex-col relative">
+        <main className="relative flex bg-black-3">
+           <LeftSideBar />
+           <section className="border-2 flex min-h-screen flex-1 flex-col px-4 sm:px-14">
+            <div className="mx-auto flex w-full max-w-5xl flex-col max-sm:px-4">
+              <div className="flex h-16 items-center justify-between md:hidden">
+                <Image 
+                  src="/icons/logo.svg" 
+                  width={30} 
+                  height={30} 
+                  alt="menu icon"/>
+                <MobileNav />
+              </div>
+              <div className="flex flex-col md:pd-14">
+                Toaster (notification popups)
+                {children}
+              </div>
+            </div>
+           </section>
+           <RightSideBar />
+
         </main>
       </div>
     );
