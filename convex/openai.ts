cfa5262f87e658ef247dc47c4957a11e0d9d75b4
xiayulin123 <xiayulinsky@gmail.com>
2024-10-01 21:22:28 -0400

    adding the generating audio function

---
+++ convex/openai.ts
@@ -0,0 +1,22 @@
+import { action } from "./_generated/server";
+import { v } from "convex/values";
+import OpenAI from "openai"
+import { SpeechCreateParams } from "openai/resources/audio/speech.mjs";
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+export const generatedAudioAction = action({
+  args: { input: v.string(), voice: v.string() },
+  handler: async (_, {voice, input}) => {
+    // call open ai api
+    const audio = await openai.audio.speech.create({
+        model:"tts-1",
+        voice: voice as SpeechCreateParams['voice'],
+        input,
+    });
+    console.log()
+    return "success";
+  },
+});
