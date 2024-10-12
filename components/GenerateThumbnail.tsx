5d54752933d731e1ac2a4a8208a11ac51260f1bc
xiayulin123 <xiayulinsky@gmail.com>
2024-10-12 13:47:50 -0400

    adding the generate Thumbnail component

--- components/GenerateThumbnail.tsx
+++ components/GenerateThumbnail.tsx
@@ -1,12 +1,67 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button } from './ui/button'
 import { cn } from '@/lib/utils';
 import { Textarea } from './ui/textarea'
 import { Label } from './ui/label'
+import { GenerateThumbnailProps } from '@/types';
+import { Loader } from 'lucide-react';
+import { Input } from './ui/input';
+import Image from 'next/image';
+import { useToast } from '@/hooks/use-toast';
+import { useMutation } from 'convex/react';
+import { useUploadFiles } from '@xixixao/uploadstuff/react';
+import { api } from '@/convex/_generated/api';
 
-
-const GenerateThumbnail = ({ setImage, setImageStorageId, image}) => {
+const GenerateThumbnail = ({ setImage, setImageStorageId, image, imagePrompt, setImagePrompt} : GenerateThumbnailProps) => {
   const [isAiThubmbnail, setIsAiThumbnail] = useState(false);
+  const [isImageLoading, setIsImageLoading] = useState(false)
+  const imageRef = useRef<HTMLInputElement>(null)
+  const { toast } = useToast()
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl)
+  const { startUpload } = useUploadFiles(generateUploadUrl)
+  const getImageUrl = useMutation(api.podcasts.gerUrl)
+
+
+  const handleImage =  async(blob: Blob, fileName: string) => {
+    setIsImageLoading(true);
+    setImage('')
+    try {
+      const file = new File([blob], fileName, { type: `image/png`})
+
+      const uploaded = await startUpload([file])
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      const storageId = (uploaded[0].response as any).storageId
+
+      setImageStorageId(storageId)
+
+      const imageUrl = await getImageUrl({ storageId })
+      setImage(imageUrl!)
+      setIsImageLoading(false);
+      toast({
+        title: "Generating the thumbnail successfully", variant: 'destructive'
+      })
+
+    } catch (error) {
+      console.log(error)
+      toast({
+        title: "Error generate the thumbnail", variant: 'destructive'
+      })
+    }
+  }
+  const generateImage = async () => {
+
+  }
+  const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault()
+    try {
+      
+    } catch (error) {
+      console.log(error)
+      toast({
+        title: "Error upload the image", variant: 'destructive'
+      })
+    }
+  }
   return (
     <>
      <div className='generate_thumbnail'>
@@ -21,23 +76,23 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image}) => {
     
 
     {isAiThubmbnail ? (
-      <div>
-<div className='flex flex-col gap-2.5'>
+      <div className='flex flex-col gap-5'>
+<div className='mt-5 flex flex-col gap-2.5'>
         <Label className="text-16 font-bold text-white-1">
-          AI Prompt to generate podcast
+          AI Prompt to generate Thumbnail
         </Label>
         <Textarea 
         className='input-class font-light focus-visible:ring-offset-orange-1'
-        placeholder='Provide text to generate the audio'
+        placeholder='Provide text to generate the thumbnail'
         rows={5}
-        value={props.voicePrompt}
-        onChange={(e)=> props.setVoicePrompt(e.target.value)}
+        value={imagePrompt}
+        onChange={(e)=> setImagePrompt(e.target.value)}
         />
       </div>
-      <div className='mt-5 w-full max-w-[200px]'>
+      <div className='w-full max-w-[200px]'>
       <Button type="submit" className="text-16 bg-orange-1 py-4 font-bold text-white-1"
-      onClick={generatePodcast}>
-        {isGenerating ? (
+      onClick={generateImage} onChange={(e)=>uploadImage(e)}>
+        {isImageLoading ? (
           <>
             Generating
             <Loader size={20} className="animate-spin ml-3" />
@@ -51,11 +106,34 @@ const GenerateThumbnail = ({ setImage, setImageStorageId, image}) => {
       </div>
       </div>
     ) : (
-      <div>
+      <div className='image_div' onClick={()=>imageRef ?.current?.click()}>
+        <Input type='file' className='hidden' ref={imageRef}/>
+        {!isImageLoading ? (
+          <Image src="/icons/upload-image.svg" width={40} height={40} alt="upload"/>
+        ) : (<div className='text-16 flex-center font-medium text-white-1'> 
+          Uploading
+          <Loader size={20} className="animate-spin ml-3" />
+
+          </div>)}
+          <div className='flex flex-col items-center gap-1'>
+      <h2 className='text-12 font-bold text-orange-1'>
+        Click to upload
+      </h2>
+      <p className='text-12 font-normal text-gray-1'>
+        SVG, PNG, JPG, or GIF (max. 1080x1080px)
+      </p>
+
+    </div>
         </div>
     )}
-
     
+
+    {image && (
+      <div className='flex-center w-full'>
+        <Image src={image} width={200} height={200} className='mt-5' alt='thumbnail' />
+
+      </div>
+    )}
     </>
    
   )
